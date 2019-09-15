e96b633ed57ddeb4124ec1c5aa8b7626b75ad47e
Keith Steckert <keith@analyticowl.com>
2019-09-15 01:56:05 -0400

    pretty basic

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # ring-pi
+
+This project is based on [@dgreif/ring-alarm](https://www.npmjs.com/package/@dgreif/ring-alarm)
+
+It is a very basic CLI tool for arming a Ring home security system in HOME or AWAY mode, and for DISARMING.
+
+- Automatically arm your system to `HOME` every night
+- Setup custom rules that interact with your smart home setup
+- Sitting at your computer, want to arm the alarm, but you're too lazy to get up and your phone is across the room?  Just run `ring home`.
+- Use your imagination
+
+`ring [status|home|away|disarm] [auth [srv]]`
+
+## status
+This will show the current state of your ring alarm.
+
+## auth
+If an auth.json file is not present, the `auth` flag defaults to `true`.
+
+This will prompt for your Ring username/password, and will then save the refreshToken to `auth.json` in your project directory.
+
+Future calls to the app will connect automatically using the token in `auth.json`.
+
+## srv
+If you will be running this on a separate server (eg: Raspberry Pi), this argument will prompt you for server information (host/dir/user/pass) and save all but the password to 'srv.json'.
+
+Whenever you run this script locally with the auth flag, a copy of `auth.json` will be copied to this server at the path specified.  Once `srv.json` exists, you will be prompted for ONLY the password in the future when using the `auth` flag.
+
+The `refreshToken` will expire every so often (~ 2 weeks).  This is goverened by Ring.  As far as I know Ring is a closed system, so there isn't a way to get a permanent API key.  I run this on my system once a week with the `auth` flag to push a new refreshToken to my Raspberry Pi.
+
+Do you want to update the token w/o arming/disarming, run it with `status`:
+```
+ring status auth [srv]
+```
+
+This does not yet support certificate based authentication.  Feel free to add that support if you need it.
---
+++ app.js
@@ -0,0 +1,139 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+let readline = require('readline')
+var scp2 = require('scp2')
+var args = require('minimist')(process.argv.slice(2))
+var { RingApi } = require('@dgreif/ring-alarm')
+
+var srv = fs.existsSync('./srv.json') ? require('./srv.json') : false
+var auth = fs.existsSync('./auth.json') ? require('./auth.json') : false
+var ring
+var home
+
+if (!auth) args.auth = true
+
+async function updateToken(){
+  let pass_auth = {}
+  pass_auth.email = await prompt(`e-mail address: `)
+  pass_auth.password = await prompt(`password: `)
+  return pass_auth
+}
+
+function prompt(question = ''){
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  return new Promise ((resolve) => {
+    rl.question(question, (response) => {
+      resolve(response)
+      rl.close()
+    })
+  })
+}
+
+async function copyTokenToServer(){
+  if (!srv) srv = {}
+
+  console.log('')
+  console.log('Copying refreshToken to server...')
+  let host = srv.server || await prompt('dest host/addr: ')
+  if (srv.server) console.log('host:', host)
+  let dirpath = srv.path || await prompt('project path on dest: ')
+  if (srv.path) console.log('project path (on dest):', dirpath)
+  let username = srv.username || await prompt('username: ')
+  if (srv.username) console.log('user:', username)
+  let password = await prompt('password: ')
+  let copyResults = await copySCP(username, password, host, dirpath)
+
+  if (args.srv || getArg('srv')){
+    let cfg = {server: host, path: dirpath, username}
+    let cfg_json = JSON.stringify(cfg, null, 2)
+    fs.writeFileSync('srv.json', cfg_json)
+  }
+
+  return copyResults
+}
+
+function copySCP(u, p, h, d){
+  return new Promise(resolve => {
+    scp2.scp('auth.json', `${u}:${p}@${h}:${d}/auth.json`, err => {
+      let msg
+      if (err) {
+        msg = 'The new refreshToken was NOT uploaded to the Raspberry Pi.'
+      } else {
+        msg = 'The refreshToken has been updated on Raspberry Pi'
+      }
+      console.log('')
+      resolve(msg)
+    })
+  })
+}
+
+function getArg(w = ''){
+  if (args[w.toLowerCase()]) return true
+
+  let words = args._.map(i => i.toString().toLowerCase())
+  let found = words.indexOf(w) != -1
+  return found
+}
+
+async function main() {
+
+  let promptForCreds = args.auth || getArg('auth')
+  if (promptForCreds) auth = await updateToken()
+
+  try {
+    ring = new RingApi(auth)
+    let locations = await ring.getLocations()
+    home = locations[0]
+  } catch (error) {
+    console.error('Failed To Connect!')
+    console.error(error)
+    process.exit()
+  }
+
+  if (promptForCreds) {
+    let token = {refreshToken: ring.restClient.refreshToken}
+    let token_json = JSON.stringify(token, null, 2)
+    fs.writeFileSync('auth.json', token_json)
+    if (srv || args.srv || getArg('srv')) {
+      let copyStatus =  await copyTokenToServer()
+      console.log(copyStatus)
+    }
+  }
+
+  let exitAfterConfirm = args.status || getArg('status')
+
+  await home.getSecurityPanel()
+  home.securityPanel.onData.subscribe(data => {
+    let statuses = {
+      all:  'AWAY',
+      some: 'HOME',
+      none: 'DISARMED'
+    }
+    status = statuses[data.mode]
+    console.log(`Alarm Mode: ${status}`)
+    if (exitAfterConfirm) process.exit()
+  })
+
+  if (getArg('home')) {
+    console.log('Arming...')
+    exitAfterConfirm = true
+    await home.armHome()
+  } else if (getArg('away')) {
+    console.log('Arming...')
+    exitAfterConfirm = true
+    await home.armAway()
+  } else if (getArg('disarm')) {
+    console.log('Disarming...')
+    exitAfterConfirm = true
+    await home.disarm()
+  }
+
+  return
+}
+main()
+
